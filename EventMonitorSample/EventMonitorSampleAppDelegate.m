ff07611557f5b2044acffa09ec7658de2f33bc31
Hiroshi Hashiguchi <xcatsan@mac.com>
2011-02-04 11:26:53 +0900

    Updated Event Monitor Sample

--- EventMonitorSample/EventMonitorSampleAppDelegate.m
+++ EventMonitorSample/EventMonitorSampleAppDelegate.m
@@ -8,12 +8,30 @@
 
 #import "EventMonitorSampleAppDelegate.h"
 
+#import <AvailabilityMacros.h>
+#import <CoreFoundation/CoreFoundation.h>
+#import <ApplicationServices/ApplicationServices.h>
+
 @implementation EventMonitorSampleAppDelegate
 
 @synthesize window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application 
+	NSString* executablePath = [[NSBundle mainBundle] executablePath];
+	int error = AXMakeProcessTrusted((CFStringRef)executablePath);
+	if(error != kAXErrorSuccess)
+	{
+		NSLog(@"*** Could not make process trusted! Path: %@ Error:%i ***",executablePath,error);
+		
+		//fallback: ask the user to enable access for assistive devices manually
+		int result = NSRunAlertPanel(@"Enable Access for Assistive Devices." , @"To continue, please enable access for assistive devices in the Universal Access pane in System Preferences. Then, relaunch the application." , @"Open System Preferences", @"Quit", nil);
+		
+		if(result == NSAlertDefaultReturn)
+		{
+			[[NSWorkspace sharedWorkspace]openFile:@"/System/Library/PreferencePanes/UniversalAccessPref.prefPane"];
+		}
+	}
 	
 	[NSEvent addGlobalMonitorForEventsMatchingMask:NSKeyDownMask
 										   handler:^(NSEvent* event) {
